548793418874ff767eb6838dcc942a3b40a3d9d9
John <cj592510@gmail.com>
2025-12-19 15:01:28 +0530

    Implement basic blackjack game functionality

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+
+const messageEl = document.getElementById("message-el")
+const cardsEl = document.getElementById("cards-el")
+const sumEl = document.getElementById("sum-el")
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randomNumber === 1) {
+        return 11 // Ace
+    } else if (randomNumber > 10) {
+        return 10 // Face cards
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    hasBlackJack = false
+
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "🎉 You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "💀 You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
 
