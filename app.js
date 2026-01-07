03018447c70d7cb225d3bf6e109b8b03cce4b477
John <cj592510@gmail.com>
2026-01-07 21:29:33 +0530

    Fix formatting issues in getRandomCard function

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
 
     if (randomNumber === 1) {
-        return 11 // Ace
+        return 11 
     } else if (randomNumber > 10) {
-        return 10 // Face cards
+        return 10 
     } else {
         return randomNumber
     }
